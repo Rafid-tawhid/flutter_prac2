52c5ee99a51d4614a33ee058bf361feb1bb6363f
Tawhidur Rahman Rafid <ilovemymom12345>
2021-04-24 02:05:46 +0600

    Listtile prac

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Scaffold(
+        body: ListView(
+          children: <Widget>[
+
+            ListTile(
+              title: Text("Rafid Tawhid"),
+              subtitle: Text(" I am a totally ajira public"),
+              leading: CircleAvatar(child: Icon(Icons.mediation),),
+              trailing: CircleAvatar(child: Icon(Icons.add_a_photo),),
+
+            ),
+            ListTile(
+              title: Text("Rafid Tawhid"),
+              subtitle: Text(" I am a totally ajira public"),
+              leading: CircleAvatar(child: Icon(Icons.mediation),),
+              trailing: CircleAvatar(child: Icon(Icons.add_a_photo),),
+
+            ),
+            ListTile(
+              title: Text("Rafid Tawhid"),
+              subtitle: Text(" I am a totally ajira public"),
+              leading: CircleAvatar(child: Icon(Icons.mediation),),
+              trailing: CircleAvatar(child: Icon(Icons.add_a_photo),),
+
+            ),
+
+          ],
+        ),
+      )
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          height: MediaQuery.of(context).size.height / 5,
-          width: MediaQuery.of(context).size.height / 4,
-          color: Colors.blue,
-        ),
-      ),
-    );
-  }
-}
+
