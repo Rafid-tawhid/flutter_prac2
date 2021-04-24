96f846cee784ba6f1559532574ed1bdeb057de13
Tawhidur Rahman Rafid <ilovemymom12345>
2021-04-25 05:17:54 +0600

    TabBar flutter

--- lib/main.dart
+++ lib/main.dart
@@ -16,46 +16,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
 
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: DefaultTabController(length: 2, child: Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(tabs: [
+          Tab(icon: Icon(Icons.camera),),
+          Tab(icon: Icon(Icons.video_call),),
+        ],
+
+        ),
+      ),
+        body: TabBarView(children: [
+          Page1(),
+          Page2()
+        ],),
+
+      )
+      ),
 
     );
   }
 }
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var current_index=0;
-  final pages=[
-    Page1(),
-    Page2(),
-    Page3(),
-    Page4()
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        bottomNavigationBar: BottomNavigationBar(currentIndex:current_index,items: [
-        BottomNavigationBarItem(backgroundColor: Colors.blueAccent,
-        icon: Icon(Icons.message),title: Text("Message")),
-    BottomNavigationBarItem(icon: Icon(Icons.call),title: Text("Call")),
-    BottomNavigationBarItem(icon: Icon(Icons.mail),title: Text("Mail")),
-    BottomNavigationBarItem(icon: Icon(Icons.sms),title: Text("SMS")),
-    ],
-
-        onTap: (index){
-          setState(() {
-
-            current_index=index;
-          });
-        },
-        ),
-    body: pages[current_index]);
-  }
-}
-
-
-
-
